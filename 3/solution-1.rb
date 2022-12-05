1e938b40986b638ba30802afc905e57fc2c297e5
armandfardeau <fardeauarmand@gmail.com>
2022-12-05 23:26:53 +0100

    Use a shorter syntax for file reading

--- 3/solution-1.rb
+++ 3/solution-1.rb
@@ -1,1 +1,1 @@
-puts File.read(File.expand_path("input", File.dirname(__FILE__))).split("\n").map{_1.each_char.each_slice(_1.size/2).to_a}.map{(_1[0] & _1[1])[0]}.map{(('a'..'z').to_a + ('A'..'Z').to_a).find_index(_1) + 1}.sum
+puts File.read("input").split("\n").map{_1.each_char.each_slice(_1.size/2).to_a}.map{(_1[0] & _1[1])[0]}.map{(('a'..'z').to_a + ('A'..'Z').to_a).find_index(_1) + 1}.sum
--- 4/solution-1.rb
+++ 4/solution-1.rb
@@ -1,1 +1,1 @@
-puts File.read(File.expand_path("input", File.dirname(__FILE__))).split("\n").map{_1.split(",").map{|r| Range.new(*r.split("-").map(&:to_i))}}.map{_1[0].cover?(_1[1]) || _1[1].cover?(_1[0])}.count{_1}
+puts File.read("input").split("\n").map{_1.split(",").map{|r| Range.new(*r.split("-").map(&:to_i))}}.map{_1[0].cover?(_1[1]) || _1[1].cover?(_1[0])}.count{_1}

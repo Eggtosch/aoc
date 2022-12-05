72df632fe8c9b607508d942f5619e2488ac57bb3
Stefan Wrobel <swrobel@users.noreply.github.com>
2022-12-05 14:40:17 -0800

    Use #max(n) to simplify

--- 1/solution-2.rb
+++ 1/solution-2.rb
@@ -1,1 +1,1 @@
-puts File.read(File.expand_path("input", File.dirname(__FILE__))).split("\n\n").map{_1.split.map(&:to_i).sum}.sort.reverse[..2].sum
+puts File.read(File.expand_path("input", File.dirname(__FILE__))).split("\n\n").map{_1.split.map(&:to_i).sum}.max(3).sum

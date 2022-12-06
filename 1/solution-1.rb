6f5d4bb0b36527735cb89b9b1fc47c0bb3115357
armandfardeau <fardeauarmand@gmail.com>
2022-12-06 02:24:37 +0100

    Revert "Use a shorter syntax for file reading"
    This reverts commit 1e938b40986b638ba30802afc905e57fc2c297e5.

--- 1/solution-1.rb
+++ 1/solution-1.rb
@@ -1,1 +1,1 @@
-puts File.read("input").split("\n\n").map { _1.split.map(&:to_i).sum }.max
+puts File.read(File.expand_path("input", File.dirname(__FILE__))).split("\n\n").map{_1.split.map(&:to_i).sum}.max
--- 2/solution-2.rb
+++ 2/solution-2.rb
@@ -1,1 +1,1 @@
-puts File.read("input").split("\n").map{[[3,4,8],[1,5,9],[2,6,7]][_1[0].ord-"A".ord][_1[2].ord-"X".ord]}.sum
+puts File.read(File.expand_path("input", File.dirname(__FILE__))).split("\n").map{[[3,4,8],[1,5,9],[2,6,7]][_1[0].ord-"A".ord][_1[2].ord-"X".ord]}.sum

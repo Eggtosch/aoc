3259cfb642c713ad4b3923a1947eb3b35fb7d9fd
Oskar Munz <oskar.munz@gmx.de>
2022-12-07 16:14:38 +0100

    simplify each_with_index to find_index

--- 6/solution-1.rb
+++ 6/solution-1.rb
@@ -1,1 +1,1 @@
-File.read(File.expand_path("input", File.dirname(__FILE__))).chars.each_cons(4).with_index { |s, i| puts i + 4 or return if s.uniq.eql? s }
+puts File.read(File.expand_path("input", File.dirname(__FILE__))).chars.each_cons(4).find_index{_1.uniq.eql? _1} + 4

9fadb4eb6c3290ba1969924861c489e35409fa2c
Freddy Abrahamsson <3440472+freddyabrahamsson@users.noreply.github.com>
2022-12-07 01:32:23 +0100

    Update solution-1.rb
    Not using oneliners for my own solutions, but I managed to work this one out for today.
    
    Swap out the `4`s for `14` for part b.

--- 6/solution-1.rb
+++ 6/solution-1.rb
@@ -1,1 +1,1 @@
-puts (0..File.size(File.expand_path("input", File.dirname(__FILE__)))).to_a.map{|i| File.read(File.expand_path("input", File.dirname(__FILE__)))[i...(i+4)]}.map{_1.strip.chars.uniq.join}.find_index{_1.size >= 4} + 4
+File.read(File.expand_path("input", File.dirname(__FILE__))).chars.each_cons(4).with_index { |s, i| puts i + 4 or return if s.uniq.eql? s }
